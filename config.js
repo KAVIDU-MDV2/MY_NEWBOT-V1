a54f3a8d2431f2f4e9953807040047d2e491fdcf
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-11-08 15:37:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ module.exports = {
     MAX_RETRIES: 3,
     GROUP_INVITE_LINK: 'https://chat.whatsapp.com/JoJDgSg3CKmHdZ66YfUhpi?mode=wwc',
     ADMIN_LIST_PATH: './admin.json',
-    IMAGE_PATH: 'https://files.catbox.moe/ybxvel.jpg',
-    NEWSLETTER_JID: '120363421928318889@newsletter',
+    IMAGE_PATH: 'https://files.catbox.moe',  ///මෙතනට දාපන් img url එක 
+    NEWSLETTER_JID: '120363421928318889@n',
     NEWSLETTER_MESSAGE_ID: '428',
     OTP_EXPIRY: 300000,
     NEWS_JSON_URL: '',
-    BOT_NAME: 'LAKIYA 𝗠𝗗 MINI BOT 𝗩1',
+    BOT_NAME: 'KAVIDU 𝗠𝗗 MINI BOT 𝗩1',
     OWNER_NAME: 'LAKLIDU',
-    OWNER_NUMBER: '94721164497',
+    OWNER_NUMBER: '94721164',  ///මෙතනට උබෙ නම්බරෙ දා ගනිම් කලුවෝ 
     BOT_VERSION: '1.0.0',
-    BOT_FOOTER: '> LAKIYA 𝗠𝗗 𝗩1',
+    BOT_FOOTER: '> KAVIDU 𝗠𝗗 𝗩1',
     CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb6saRv6mYPEqp1ERR3N',
 };
